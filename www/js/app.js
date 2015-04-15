9457d3c527e8744e05c50be7e4bcbb7dd44308ac
tompascall <tompascall@gmail.com>
2015-04-15 15:29:56 +0200

    refactored description directive

--- www/js/app.js
+++ www/js/app.js
@@ -16,29 +16,4 @@ angular.module('poc', ['ionic', 'firebase'])
       StatusBar.styleDefault();
     }
   });
-})
-
-.directive('description', function () {
-  var longDescription;
-  var shortDescription;
-  return {
-    restrict: 'E',
-    scope: {
-      tutor: '='
-    },
-    templateUrl: './template/description.html',
-    controller: function ($scope) {
-      longDescription = $scope.tutor.description;
-      shortDescription = longDescription.split(/\s+/).slice(0, 4).join(' ') + '...';
-      $scope.shownDescription = shortDescription;
-      $scope.tutor.showLongDescription = false;
-    },
-    link: function (scope) {
-      scope.$watch('tutor.showLongDescription', function (newValue, oldValue) {
-         if (newValue !== oldValue) {
-          scope.shownDescription = (scope.tutor.showLongDescription) ? longDescription : shortDescription;
-        }
-      });
-    }
-  };
 });
